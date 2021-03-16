5fd1ad06dd236281504bdb79ba92643e8a5d4fd4
Karahan2002 <66959328+Karahan2002@users.noreply.github.com>
2021-03-16 23:30:33 +0100

    Add files via upload

---
+++ Question6.java
@@ -0,0 +1,18 @@
+package day_2;
+
+public class Question6 {
+	public static void main(String[] args) {
+		System.out.println("   ^");
+		System.out.println("  / \\");
+		System.out.println(" /   \\");
+		System.out.println("/     \\");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("|     |");
+		System.out.println("_______");
+	}
+}
